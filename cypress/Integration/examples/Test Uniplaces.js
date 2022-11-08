c44956c54a7dda8e97c7df9256db1ab2c7cbd58b
Jflorien <joaordmp@gmail.com>
2022-11-08 01:41:15 +0000

    New

---
+++ cypress/Integration/examples/Test Uniplaces.js
@@ -0,0 +1,16 @@
+
+
+describe('My first Test', function()
+{
+
+it('My First Test', function() {
+
+cy.visit("https://staging-uniplaces.com")
+cy.wait(4000)
+
+
+
+
+} )
+
+} )
--- cypress/Integration/examples/Test1.js
+++ cypress/Integration/examples/Test1.js
@@ -1,16 +1,25 @@
 ///<reference types="Cypress" />
 
-describe('My first Test Suite', function()
+describe('My first Test Suite',function()
 {
 
-it('My FirstTest case', function() {
+it('My FirstTest case',function() {
 
-cy.visit("http://google.com")
+cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+cy.get('.search-keyword').type('ca')
+cy.wait(2000)
+cy.get('.product:visible').should('have.length',4)
 
+cy.get('.products').find('.product').should('have.length',4)
+cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
 
+cy.get('.products').find('.product')
+cy.get('.products').find('.product') .each(($el, index, $list) => {
 
-//fixture
+const textVeg=$el.find('h4.product-name').text()
+if(textVeg.includes('Cashews'))
+{
+cy.wrap($el).find('button').click()
 
-} )
-
+} 
 } )
